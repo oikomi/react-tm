62a87897c9df68e139882d8baa90222f01e1ec25
oikomi <miaohong@miaohong.org>
2025-08-22 18:19:02 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 
-import {HashRouter as Router} from 'react-router-dom';
-import {App as AntApp, ConfigProvider, theme} from 'antd';
+import { HashRouter as Router } from 'react-router-dom';
+import { App as AntApp, ConfigProvider, theme } from 'antd';
 
-import {AliveScope} from 'react-activation';
+import { AliveScope } from 'react-activation';
 import zhCN from 'antd/es/locale/zh_CN';
 import enUS from 'antd/es/locale/en_US';
 
 // antd主题
-const {defaultAlgorithm, darkAlgorithm} = theme;
+const { defaultAlgorithm, darkAlgorithm } = theme;
 
 function App() {
-  const {i18n} = useTranslation();
+  const { i18n } = useTranslation();
   const currentLanguage = i18n.language;
 
   return (
