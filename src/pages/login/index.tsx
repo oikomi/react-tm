06457b783079f1675b485b2de0a85ac3d8be3f6b
oikomi <miaohong@miaohong.org>
2025-08-24 17:22:36 +0800

    init

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -29,42 +29,50 @@ const Login = () => {
   return (
     <>
       {contextHolder}
-      <h1 className={styles.container}>{t('login.login')}</h1>
 
-      <Form
-        name="login"
-        initialValues={{ remember: true }}
-        style={{ maxWidth: 360 }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: 'Please input your Username!' }]}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Please input your Password!' }]}
-        >
-          <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-        </Form.Item>
-        <Form.Item>
-          <Flex justify="space-between" align="center">
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-            <a href="">Forgot password</a>
-          </Flex>
-        </Form.Item>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <div className={styles.left}>left</div>
+          <div className={styles.right}>
+            <h1>{t('login.login')}</h1>
+            <Form
+              className={styles.form}
+              name="login"
+              initialValues={{ remember: true }}
+              // style={{ maxWidth: 360 }}
+              onFinish={onFinish}
+            >
+              <Form.Item
+                name="username"
+                rules={[{ required: true, message: 'Please input your Username!' }]}
+              >
+                <Input prefix={<UserOutlined />} placeholder="Username" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[{ required: true, message: 'Please input your Password!' }]}
+              >
+                <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+              </Form.Item>
+              <Form.Item>
+                <Flex justify="space-between" align="center">
+                  <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                  </Form.Item>
+                  <a href="">Forgot password</a>
+                </Flex>
+              </Form.Item>
 
-        <Form.Item>
-          <Button block type="primary" htmlType="submit">
-            Log in
-          </Button>
-          or <a href="">Register now!</a>
-        </Form.Item>
-      </Form>
+              <Form.Item>
+                <Button block type="primary" htmlType="submit">
+                  Log in
+                </Button>
+                or <a href="">Register now!</a>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
